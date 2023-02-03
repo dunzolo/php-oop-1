efc4d4f50b8401a82bc7c961edf8c00e1f899bda
dunzolo <rossidavide108@gmail.com>
2023-02-03 14:53:14 +0100

    exercise done

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php
-    
+    // definisco una classe
+    class Movie{
+        public $title;
+        public $director;
+        public $language;
+        public $year;
+        public $age_movie;
+
+        //definisco il costruttore 
+        function __construct($_title, $_director, $_language, $_year){
+            //con il this faccio riferimento all'istanza creata
+            $this -> title = $_title;
+            $this -> director = $_director;
+            $this -> language = $_language;
+            $this -> year = $_year;
+        }
+
+        // calcolo l'eta del film
+        public function setAge($age){
+            $current_year = date('Y');
+            $this -> age_movie = $current_year - $age;
+        }
+
+        // permetto la visualizzazione a schermo
+        public function getAge(){
+            return $this -> age_movie;
+        }
+    }
+
+    // creo gli oggetti dell'istanza Movie, passando parametri obbligatori del costruttore
+    $movie_1 = new Movie('Titolo-1', 'Regista-1', 'Lingua-1', 1980);
+    $movie_2 = new Movie('Titolo-2', 'Regista-2', 'Lingua-2', 2000);
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +43,32 @@
         <title>PHP Primo Esercizio OOP</title>
     </head>
     <body>
-        
+        <div class="container mt-5">
+            <div class="row text-center">
+                <h1 class="text-danger">PHP Primo Esercizio OOP</h1>
+                <div class="col-12 mt-3">
+                    <h2>Primo film</h2>
+                    <?php
+                        echo "Il titolo del film è: ".$movie_1 -> title."<br>";
+                        echo "Il regista del film è: ".$movie_1 -> director."<br>";
+                        echo "Il lingua originale del film è: ".$movie_1 -> language."<br>";
+                        echo "L'anno in cui è stato prodotto il film è: ".$movie_1 -> year."<br>";
+                        $movie_1 -> setAge($movie_1 -> year);
+                        echo "Il film è stato prodotto: ".$movie_1 -> getAge()." anni fa.";
+                    ?>
+                </div>   
+                <div class="col-12 mt-3">
+                    <h2>Secondo film</h2>
+                    <?php
+                        echo "Il titolo del film è: ".$movie_2 -> title."<br>";
+                        echo "Il regista del film è: ".$movie_2 -> director."<br>";
+                        echo "Il lingua originale del film è: ".$movie_2 -> language."<br>";
+                        echo "L'anno in cui è stato prodotto il film è: ".$movie_2 -> year."<br>";
+                        $movie_2 -> setAge($movie_2 -> year);
+                        echo "Il film è stato prodotto: ".$movie_2 -> getAge()." anni fa.";
+                    ?>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
